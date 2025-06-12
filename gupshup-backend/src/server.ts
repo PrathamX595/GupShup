b388592facade2291960f3f70b8225ecf7e32c6c
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2025-06-12 21:04:02 +0530

    added lint

--- gupshup-backend/src/server.ts
+++ gupshup-backend/src/server.ts
@@ -9,12 +9,14 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 2000;
 
-app.get("/", (request: Request,response: Response) => { 
+app.get("/", (request: Request, response: Response) => {
   response.status(200).send("Hello World");
-}); 
-
-app.listen(PORT, () => { 
-  console.log("Server running at PORT: ", PORT); 
-}).on("error", (error) => {
-  throw new Error(error.message);
 });
+
+app
+  .listen(PORT, () => {
+    console.log("Server running at PORT: ", PORT);
+  })
+  .on("error", (error) => {
+    throw new Error(error.message);
+  });
