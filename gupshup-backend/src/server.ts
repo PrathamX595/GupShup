65e903864448f06f49fc05e11dd7167d502217e9
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2025-06-12 20:28:07 +0530

    backend init

---
+++ gupshup-backend/src/server.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 2000;
+
+app.get("/", (request: Request,response: Response) => { 
+  response.status(200).send("Hello World");
+}); 
+
+app.listen(PORT, () => { 
+  console.log("Server running at PORT: ", PORT); 
+}).on("error", (error) => {
+  throw new Error(error.message);
+});
