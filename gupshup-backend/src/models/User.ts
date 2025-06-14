81b19d529ee1d5c9a2741faad20daf4a06c162de
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2025-06-14 18:06:37 +0530

    added user model methods
    Signed-off-by: PrathamX595 <pratham.seth.cer23@itbhu.ac.in>

--- gupshup-backend/src/models/User.ts
+++ gupshup-backend/src/models/User.ts
@@ -1,5 +1,7 @@
 import mongoose, { Document, Schema, Model } from "mongoose";
 import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+import jwt, { SignOptions } from "jsonwebtoken";
 
 interface IUser extends Document {
   _id: string;
@@ -8,11 +10,12 @@ interface IUser extends Document {
   avatar: string;
   createdAt: Date;
   password: string;
+  refreshToken: string;
 }
 
 const UserSchema: Schema<IUser> = new Schema({
   _id: { type: String, default: uuidv4 },
-  userName: { type: String, required: true, lowercase: true, trim: true },
+  userName: { type: String, required: true, trim: true },
   password: { type: String, required: [true, "password is required"] },
   email: {
     type: String,
@@ -24,7 +27,54 @@ const UserSchema: Schema<IUser> = new Schema({
   },
   avatar: { type: String },
   createdAt: { type: Date, default: Date.now },
+  refreshToken: { type: String },
 });
+
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.isPasswordCorrect = async function (password: string) {
+  return bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = function () {
+  const payload = {
+    _id: this._id,
+    userName: this.userName,
+    email: this.email,
+  };
+
+  const secret = process.env.ACCESS_TOKEN_SECRET;
+  if (!secret) {
+    throw new Error("access token secret is not defined");
+  }
+
+  const options: SignOptions = {
+    expiresIn: process.env.ACCESS_TOKEN_EXPIRY as any,
+  };
+  return jwt.sign(payload, secret, options);
+};
+
+UserSchema.methods.generateRefreshToken = function (){
+  const payload = {
+    _id: this._id,
+    userName: this.userName,
+    email: this.email,
+  };
+
+  const secret = process.env.REFRESH_TOKEN_SECRET;
+  if (!secret) {
+    throw new Error("refresh token secret is not defined");
+  }
+
+  const options: SignOptions = {
+    expiresIn: process.env.REFRESH_TOKEN_EXPIRY as any,
+  };
+  return jwt.sign(payload, secret, options);
+}
 
 const User: Model<IUser> = mongoose.model<IUser>("User", UserSchema);
 
