8c0be465cef0215cb8e86fae4a06221f82d3cbfa
PrathamX595 <pratham.seth.cer23@itbhu.ac.in>
2025-09-15 14:12:17 +0530

    compose
    Signed-off-by: PrathamX595 <pratham.seth.cer23@itbhu.ac.in>

--- gupshup-backend/src/config/db.ts
+++ gupshup-backend/src/config/db.ts
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
+    await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log("MongoDB connected DBHost");
   } catch (error) {
     console.log("mongoDB connection error", error);
